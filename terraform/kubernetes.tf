58b3962dd774955a12613cb08fcb1eab5cf016d5
dav1dli <dav1dli@netscape.net>
2024-05-27 21:16:20 +0200

    reenable resources

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,83 +1,83 @@
-# resource "yandex_kubernetes_cluster" "k8s-cluster" {
-#   description = "Managed Service for Kubernetes cluster"
-#   name        = local.k8s_name
-#   network_id  = yandex_vpc_network.network.id
+resource "yandex_kubernetes_cluster" "k8s-cluster" {
+  description = "Managed Service for Kubernetes cluster"
+  name        = local.k8s_name
+  network_id  = yandex_vpc_network.network.id
 
-#   master {
-#     version = local.k8s_version
-#     zonal {
-#       zone      = yandex_vpc_subnet.subnet-mngmt.zone
-#       subnet_id = yandex_vpc_subnet.subnet-mngmt.id
-#     }
+  master {
+    version = local.k8s_version
+    zonal {
+      zone      = yandex_vpc_subnet.subnet-mngmt.zone
+      subnet_id = yandex_vpc_subnet.subnet-mngmt.id
+    }
 
-#     public_ip = false
+    public_ip = false
     
 
-#     master_logging {
-#       enabled                = true
-#       log_group_id           = yandex_logging_group.logs.id
-#       kube_apiserver_enabled = true
-#       cluster_autoscaler_enabled = true
-#       events_enabled             = true
-#     }
-#   }
-#   service_account_id      = yandex_iam_service_account.sa-resedit.id # Cluster service account ID.
-#   node_service_account_id = yandex_iam_service_account.sa-imgpull.id # Node group service account ID.
-#   labels = {
-#     environment = lower(var.environment)
-#   }
-#   release_channel = "REGULAR"
-#   kms_provider {
-#     key_id = "${yandex_kms_symmetric_key.k8s_enc_key.id}"
-#   }
-#   depends_on = [
-#     yandex_resourcemanager_folder_iam_binding.editor,
-#     yandex_resourcemanager_folder_iam_binding.images-puller
-#   ]
-# }
+    master_logging {
+      enabled                = true
+      log_group_id           = yandex_logging_group.logs.id
+      kube_apiserver_enabled = true
+      cluster_autoscaler_enabled = true
+      events_enabled             = true
+    }
+  }
+  service_account_id      = yandex_iam_service_account.sa-resedit.id # Cluster service account ID.
+  node_service_account_id = yandex_iam_service_account.sa-imgpull.id # Node group service account ID.
+  labels = {
+    environment = lower(var.environment)
+  }
+  release_channel = "REGULAR"
+  kms_provider {
+    key_id = "${yandex_kms_symmetric_key.k8s_enc_key.id}"
+  }
+  depends_on = [
+    yandex_resourcemanager_folder_iam_binding.editor,
+    yandex_resourcemanager_folder_iam_binding.images-puller
+  ]
+}
 
-# resource "yandex_kubernetes_node_group" "k8s-node-group" {
-#   description = "Node group for the Managed Service for Kubernetes cluster"
-#   name        = "${local.k8s_name}-node-group"
-#   cluster_id  = yandex_kubernetes_cluster.k8s-cluster.id
-#   version     = local.k8s_version
+resource "yandex_kubernetes_node_group" "k8s-node-group" {
+  description = "Node group for the Managed Service for Kubernetes cluster"
+  name        = "${local.k8s_name}-node-group"
+  cluster_id  = yandex_kubernetes_cluster.k8s-cluster.id
+  version     = local.k8s_version
 
-#   scale_policy {
-#     fixed_scale {
-#       size = 1
-#     }
-#   }
+  scale_policy {
+    fixed_scale {
+      size = 1
+    }
+  }
 
-#   allocation_policy {
-#     location {
-#       zone = yandex_vpc_subnet.subnet-mngmt.zone
-#     }
-#   }
+  allocation_policy {
+    location {
+      zone = yandex_vpc_subnet.subnet-mngmt.zone
+    }
+  }
 
-#   instance_template {
-#     platform_id = "standard-v3" # Intel Cascade Lake
+  instance_template {
+    platform_id = "standard-v3" # Intel Cascade Lake
 
-#     network_interface {
-#       nat                = true
-#       subnet_ids         = [yandex_vpc_subnet.subnet-mngmt.id]
-#     #   security_group_ids = [yandex_vpc_security_group.k8s-main-sg.id]
-#     }
+    network_interface {
+      nat                = true
+      subnet_ids         = [yandex_vpc_subnet.subnet-mngmt.id]
+    #   security_group_ids = [yandex_vpc_security_group.k8s-main-sg.id]
+    }
 
-#     resources {
-#       memory = 4 # GB
-#       cores  = 2 # Number of CPU cores.
-#     }
+    resources {
+      memory = 4 # GB
+      cores  = 2 # Number of CPU cores.
+    }
 
-#     boot_disk {
-#       type = "network-hdd"
-#       size = 64
-#     }
-#     scheduling_policy {
-#       preemptible = false
-#     }
+    boot_disk {
+      type = "network-hdd"
+      size = 64
+    }
+    scheduling_policy {
+      preemptible = false
+    }
 
-#     container_runtime {
-#       type = "containerd"
-#     }
-#   }
-# }
+    container_runtime {
+      type = "containerd"
+    }
+  }
+}
--- terraform/pgsql.tf
+++ terraform/pgsql.tf
@@ -1,115 +1,115 @@
-# resource "yandex_mdb_postgresql_cluster" "main" {
-#   name        = "pg-cluster"
-#   network_id  = yandex_vpc_network.main.id
-#   environment = "DEVELOPMENT"
-#   description = "Managed Service for PostgreSQL cluster"
-#   labels = {
-#     environment = lower(var.environment)
-#   }
-#   config {
-#     version = "16"
-#     resources {
-#       resource_preset_id = "s2.micro"
-#       disk_size          = 20
-#       disk_type_id       = "network-ssd"
-#     }
-#     postgresql_config = {
-#       max_connections                   = 395
-#       enable_parallel_hash              = true
-#       autovacuum_vacuum_scale_factor    = 0.34
-#       default_transaction_isolation     = "TRANSACTION_ISOLATION_READ_COMMITTED"
-#       shared_preload_libraries          = "SHARED_PRELOAD_LIBRARIES_AUTO_EXPLAIN,SHARED_PRELOAD_LIBRARIES_PG_HINT_PLAN"
-#     }
-#   }
-#   host {
-#     zone           = "ru-central1-a"
-#     subnet_id      = yandex_vpc_subnet.main.id
-#     assign_public_ip = false
-#   }
-# }
+resource "yandex_mdb_postgresql_cluster" "main" {
+  name        = "pg-cluster"
+  network_id  = yandex_vpc_network.main.id
+  environment = "DEVELOPMENT"
+  description = "Managed Service for PostgreSQL cluster"
+  labels = {
+    environment = lower(var.environment)
+  }
+  config {
+    version = "16"
+    resources {
+      resource_preset_id = "s2.micro"
+      disk_size          = 20
+      disk_type_id       = "network-ssd"
+    }
+    postgresql_config = {
+      max_connections                   = 395
+      enable_parallel_hash              = true
+      autovacuum_vacuum_scale_factor    = 0.34
+      default_transaction_isolation     = "TRANSACTION_ISOLATION_READ_COMMITTED"
+      shared_preload_libraries          = "SHARED_PRELOAD_LIBRARIES_AUTO_EXPLAIN,SHARED_PRELOAD_LIBRARIES_PG_HINT_PLAN"
+    }
+  }
+  host {
+    zone           = "ru-central1-a"
+    subnet_id      = yandex_vpc_subnet.main.id
+    assign_public_ip = false
+  }
+}
 
-# resource "yandex_mdb_postgresql_database" "app_db" {
-#   cluster_id = yandex_mdb_postgresql_cluster.main.id
-#   name       = "app_db"
-#   owner      = yandex_mdb_postgresql_user.app_admin.name
-#   lc_collate = "en_US.UTF-8"
-#   lc_type    = "en_US.UTF-8"
-#   extension {
-#     name = "uuid-ossp"
-#   }
-#   extension {
-#     name = "xml2"
-#   }
-# }
-# resource "random_password" "app_admin_password" {
-#   length  = 16
-#   special = true
-#   upper   = true
-#   lower   = true
-#   numeric  = true
-#   min_upper = 2
-#   min_lower = 2
-#   min_numeric = 2
-#   min_special = 2
-#   keepers = {
-#     resource_id = yandex_mdb_postgresql_cluster.main.id
-#   }
-# }
-# resource "yandex_mdb_postgresql_user" "app_admin" {
-#   cluster_id = yandex_mdb_postgresql_cluster.main.id
-#   name       = "app_admin"
-#   password   = random_password.app_admin_password.result
-#   grants = ["ALL"]
-#   permission {
-#     database_name = "app_db"
-#   }
-# }
-# resource "random_password" "app_write_password" {
-#   length  = 16
-#   special = true
-#   upper   = true
-#   lower   = true
-#   numeric  = true
-#   min_upper = 2
-#   min_lower = 2
-#   min_numeric = 2
-#   min_special = 2
-#   keepers = {
-#     resource_id = yandex_mdb_postgresql_cluster.main.id
-#   }
-# }
-# resource "yandex_mdb_postgresql_user" "app_write_user" {
-#   cluster_id = yandex_mdb_postgresql_cluster.main.id
-#   name       = "app_write_user"
-#   password   = random_password.app_write_password.result
-#   grants = ["WRITE"]
-#   permission {
-#     database_name = "app_db"
-#   }
-# }
-# resource "random_password" "app_read_password" {
-#   length  = 16
-#   special = true
-#   upper   = true
-#   lower   = true
-#   numeric  = true
-#   min_upper = 2
-#   min_lower = 2
-#   min_numeric = 2
-#   min_special = 2
-#   keepers = {
-#     resource_id = yandex_mdb_postgresql_cluster.main.id
-#   }
-# }
-# resource "yandex_mdb_postgresql_user" "app_read_user" {
-#   cluster_id = yandex_mdb_postgresql_cluster.main.id
-#   name       = "app_read_user"
-#   password   = random_password.app_read_password.result
-#   grants = ["READ"]
-#   permission {
-#     database_name = "app_db"
-#   }
-# }
-# output "pgcluster_fqdn" {
-#   value = yandex_mdb_postgresql_cluster.main.host.fqdn
-# }
+resource "yandex_mdb_postgresql_database" "app_db" {
+  cluster_id = yandex_mdb_postgresql_cluster.main.id
+  name       = "app_db"
+  owner      = yandex_mdb_postgresql_user.app_admin.name
+  lc_collate = "en_US.UTF-8"
+  lc_type    = "en_US.UTF-8"
+  extension {
+    name = "uuid-ossp"
+  }
+  extension {
+    name = "xml2"
+  }
+}
+resource "random_password" "app_admin_password" {
+  length  = 16
+  special = true
+  upper   = true
+  lower   = true
+  numeric  = true
+  min_upper = 2
+  min_lower = 2
+  min_numeric = 2
+  min_special = 2
+  keepers = {
+    resource_id = yandex_mdb_postgresql_cluster.main.id
+  }
+}
+resource "yandex_mdb_postgresql_user" "app_admin" {
+  cluster_id = yandex_mdb_postgresql_cluster.main.id
+  name       = "app_admin"
+  password   = random_password.app_admin_password.result
+  grants = ["ALL"]
+  permission {
+    database_name = "app_db"
+  }
+}
+resource "random_password" "app_write_password" {
+  length  = 16
+  special = true
+  upper   = true
+  lower   = true
+  numeric  = true
+  min_upper = 2
+  min_lower = 2
+  min_numeric = 2
+  min_special = 2
+  keepers = {
+    resource_id = yandex_mdb_postgresql_cluster.main.id
+  }
+}
+resource "yandex_mdb_postgresql_user" "app_write_user" {
+  cluster_id = yandex_mdb_postgresql_cluster.main.id
+  name       = "app_write_user"
+  password   = random_password.app_write_password.result
+  grants = ["WRITE"]
+  permission {
+    database_name = "app_db"
+  }
+}
+resource "random_password" "app_read_password" {
+  length  = 16
+  special = true
+  upper   = true
+  lower   = true
+  numeric  = true
+  min_upper = 2
+  min_lower = 2
+  min_numeric = 2
+  min_special = 2
+  keepers = {
+    resource_id = yandex_mdb_postgresql_cluster.main.id
+  }
+}
+resource "yandex_mdb_postgresql_user" "app_read_user" {
+  cluster_id = yandex_mdb_postgresql_cluster.main.id
+  name       = "app_read_user"
+  password   = random_password.app_read_password.result
+  grants = ["READ"]
+  permission {
+    database_name = "app_db"
+  }
+}
+output "pgcluster_fqdn" {
+ value = yandex_mdb_postgresql_cluster.main.host.fqdn
+}
