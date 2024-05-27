208de28e2c83dfa7f3e69b814aced4b0b0cd727b
dav1dli <dav1dli@netscape.net>
2024-05-27 21:13:27 +0200

    create jumphost

--- terraform/config.tf
+++ terraform/config.tf
@@ -5,21 +5,6 @@ terraform {
     }
   }
   required_version = ">= 0.13"
-
-  # backend "s3" {
-  #   endpoints = {
-  #     s3 = "https://storage.yandexcloud.net"
-  #   }
-  #   bucket                      = "s3tfstate"
-  #   key                         = "terraform.tfstate"
-  #   region                      = "ru-central1"
-  #   access_key                  = "YCAJELW8fC9GgxGz5FYX6NJuH"
-  #   secret_key                  = "YCPBcZYUvJ8L-lLTm2kt17e_-14YJB32QZmzKQJk"
-  #   skip_region_validation      = true
-  #   skip_credentials_validation = true
-  #   skip_requesting_account_id  = true
-  #   skip_s3_checksum            = true
-  # }
 }
 
 provider "yandex" {
